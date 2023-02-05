977ae9adfcf3ecfe136a8ef2191c70954289936e
Hans Tanawi <hans.tanawi@outlook.com>
2023-02-05 20:44:28 +0700

    fix: section mobile max width

--- src/components/layout/SectionLayout.tsx
+++ src/components/layout/SectionLayout.tsx
@@ -6,7 +6,7 @@ type SectionLayoutProps = {
 
 const SectionLayout = ({ children }: SectionLayoutProps) => {
   return (
-    <div className='my-0 mx-auto box-border relative md:max-w-6xl max-w-md w-full h-full'>
+    <div className='my-0 mx-auto box-border relative md:max-w-6xl max-w-sm w-full h-full'>
       {children}
     </div>
   );
