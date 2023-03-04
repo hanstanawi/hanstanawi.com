e778032d4ad058d022b7970d10105b107a4e9843
Hans Tanawi <hans.tanawi@outlook.com>
2023-03-04 11:28:48 +0700

    feat: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
-import Head from 'next/head';
-import '../styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
