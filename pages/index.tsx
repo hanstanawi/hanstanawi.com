0a9ffd2d895cf2370ff22b84fba10443f692b3a8
Hans Tanawi <hans.tanawi@outlook.com>
2023-03-07 21:11:34 +0700

    feat: add 404 page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,13 @@ import { NextSeo } from 'next-seo';
 import type { NextPage } from 'next';
 
 import Navbar from 'components/navigation/Navbar';
-import Footer from 'components/navigation/Footer';
 import Hero from 'features/hero';
 import About from 'features/about';
 import Experience from 'features/experience';
 import Featured from 'features/featured';
 import Projects from 'features/projects';
 import Contact from 'features/contact';
+import Footer from 'components/navigation/Footer';
 
 import { createSeoConfig } from 'lib/seo.lib';
 import { homepageSeo } from 'constants/seo.constant';
