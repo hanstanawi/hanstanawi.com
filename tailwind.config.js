929444b05a9652f684ebbfcd7b8149eb0cc619ee
Hans Tanawi <hans.tanawi@outlook.com>
2023-03-17 17:06:28 +0700

    chore: add default dark colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
       },
       colors: {
         tealGreen: '#04A2B9',
+        dark: '#23272F',
+        darkV2: '#161615',
+        darkV3: '#1F2937',
+        darkV4: '#0A1930',
+        darkV5: '#191B22',
       },
       transitionProperty: {
         top: 'top',
