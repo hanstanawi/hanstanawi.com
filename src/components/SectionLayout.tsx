f9a90d98d99321d1c478a6009c87d317fd87f363
Hans Tanawi <hans.tanawi@outlook.com>
2023-03-19 08:54:30 +0700

    fix: large mobile min width

--- src/components/SectionLayout.tsx
+++ src/components/SectionLayout.tsx
@@ -6,7 +6,7 @@ type SectionLayoutProps = {
 
 const SectionLayout = ({ children }: SectionLayoutProps) => {
   return (
-    <div className='my-0 mx-auto box-border relative xl:max-w-6xl lg:max-w-4xl sm:max-w-2xl max-w-sm sm:px-0 px-6 w-full h-full'>
+    <div className='my-0 mx-auto box-border relative xl:max-w-6xl lg:max-w-4xl md:max-w-2xl sm:max-w-lg max-w-sm sm:px-0 px-6 w-full h-full'>
       {children}
     </div>
   );
